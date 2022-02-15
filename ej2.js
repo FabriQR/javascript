59702763fab0237a8e0f44bc897c97827eb36275
FabriQR <89361874+FabriQR@users.noreply.github.com>
2022-02-15 16:28:42 +0100

    Add files via upload

---
+++ ej2.js
@@ -0,0 +1,13 @@
+numeroA = 15;
+numeroB = 24;
+
+let suma = numeroA + numeroB;
+let resta = numeroA - numeroB;
+let multiplicacion = numeroA * numeroB;
+let division = numeroA / numeroB;
+let resto = numeroA % numeroB;
+console.log(suma);
+console.log(resta);
+console.log(multiplicacion);
+console.log(division);
+console.log(resto);
